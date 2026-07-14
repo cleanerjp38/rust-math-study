654b0675624d09f1ac7374bd6ac9577a13bc0cb2
cleanerjp38 <cleanerjp38@gmail.com>
2026-07-14 14:18:52 +0900

    構造体の中に構造体の配列を入れる forで構造体の配列を参照する

---
+++ src/bin/0112_struct_test.rs
@@ -0,0 +1,43 @@
+struct Student {
+    id: u32,
+    name: String,
+}
+
+struct HairetsuTest {
+    //これでは生徒1人分の名簿にしかならない
+    //meibo: Student, 
+    meibo: Vec<Student>,
+}
+
+impl HairetsuTest {
+    //まっさらな名簿を準備
+    fn new() -> Self {
+        Self { meibo:Vec::new() }
+    }
+
+    fn add_student(&mut self, student: Student) {
+        self.meibo.push(student);
+    }
+}
+
+fn main() {
+    let mut my_test = HairetsuTest::new();
+
+    let student1 = Student {
+        id: 1,
+        name: String::from("Taro"),
+    };
+
+    let student2 = Student {
+        id: 2,
+        name: String::from("Jiro"),
+    };
+
+    my_test.add_student(student1);
+    my_test.add_student(student2);
+    println!("生徒数:{}",my_test.meibo.len());
+    
+    for i in &my_test.meibo {
+        println!("{}, {}", i.id, i.name);
+    }
+}
